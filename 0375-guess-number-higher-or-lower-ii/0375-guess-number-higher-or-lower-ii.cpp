e601ca97b7ccde78c7f2378849aeeaf28914a8d4
Ajay Patel <155143901+ajaypatelxyz@users.noreply.github.com>
2026-07-24 14:24:23 +0530

    Time: 35 ms (83.09%), Space: 11.2 MB (27.99%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
-    int solveGetMoney(int s, int e, vector<vector<int>> &dp){
-        if(s >= e){
-            return 0;
-        }
-        //Check answer already in dp or not
-        if(dp[s][e] != -1){
-            return dp[s][e];
-        }
+    // int solveGetMoney(int s, int e, vector<vector<int>> &dp){
+    //     if(s >= e){
+    //         return 0;
+    //     }
+    //     //Check answer already in dp or not
+    //     if(dp[s][e] != -1){
+    //         return dp[s][e];
+    //     }
 
-        int ans = INT_MAX;
-        for(int i = s; i <= e; i++){
-            ans = min(ans, i + max(solveGetMoney(s, i-1, dp), solveGetMoney(i+1, e, dp)));
+    //     int ans = INT_MAX;
+    //     for(int i = s; i <= e; i++){
+    //         ans = min(ans, i + max(solveGetMoney(s, i-1, dp), solveGetMoney(i+1, e, dp)));
+    //     }
+    //     dp[s][e] = ans;
+    //     return dp[s][e];
+    // }
+
+    int solveGetMoney(int n){
+        vector<vector<int>> dp(n+2, vector<int> (n+1, 0));
+
+        for(int start = n; start >= 1; start--){
+            for(int end = 1; end <= n; end++){
+
+                if(start >= end){
+                    continue;
+                }
+                int ans = INT_MAX;
+                for(int i = start; i <= end; i++){
+                    ans = min(ans, i + max(dp[start][i-1], dp[i+1][end]));
+                }
+                dp[start][end] = ans;
+            }
         }
-        dp[s][e] = ans;
-        return dp[s][e];
+        return dp[1][n];
     }
 
     int getMoneyAmount(int n) {
         int start = 1;
         int end = n;
-        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
-        int ans = solveGetMoney(start, end, dp);
+        
+        int ans = solveGetMoney(n);
         return ans;
     }
 };
