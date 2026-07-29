1b922a6a07e5f870dae1ddd77e11f9570c02eb8a
Ajay Patel <155143901+ajaypatelxyz@users.noreply.github.com>
2026-07-29 10:44:53 +0530

    Time: 0 ms (100%), Space: 10.8 MB (35.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -16,7 +16,14 @@ public:
     }
 
     vector<int> countBits(int n) {
-        vector<int> ans = solveBits(n);
-        return ans;
+        // vector<int> ans = solveBits(n);
+        // return ans;
+
+        // this is optimal approach
+        vector<int> dp(n+1, 0);
+        for(int i = 1; i <= n; i++){
+            dp[i] = (i & 1) + dp[i >> 1];
+        }
+        return dp;
     }
 };
