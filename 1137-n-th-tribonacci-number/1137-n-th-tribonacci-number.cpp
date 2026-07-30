5b58d95cb04c5d6e70615c5f85819bf10087a58c
Ajay Patel <155143901+ajaypatelxyz@users.noreply.github.com>
2026-07-30 11:22:03 +0530

    Time: 0 ms (100%), Space: 7.7 MB (85.79%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int tribonacci(int n) {
         if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+
         int x = 0;
         int y = 1;
-        if(n == 1) return y;
-        if(n == 2) return x + y;
         int z = 1;
-        int sum = x + y + z;
+        int sum = (x + y + z);
         for(int i = 3; i < n; i++){
             x = y;
             y = z;
             z = sum;
-            sum = (x + y + z);
+            sum = x + y + z;
         }
         return sum;
     }
