45e33b08e7a0ebe5f70e45941fd20a2b19baaca7
Ajay Patel <155143901+ajaypatelxyz@users.noreply.github.com>
2026-07-30 11:27:11 +0530

    Time: 0 ms (100%), Space: 8.3 MB (45.16%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int tribonacci(int n) {
+    int solve(int n, vector<int> &dp){
         if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-
-        int x = 0;
-        int y = 1;
-        int z = 1;
-        int sum = (x + y + z);
-        for(int i = 3; i < n; i++){
-            x = y;
-            y = z;
-            z = sum;
-            sum = x + y + z;
+        //check answer already exists or not
+        if(dp[n] != -1){
+            return dp[n];
         }
-        return sum;
+
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp) + solve(n - 3, dp);
+        return dp[n];
+    }
+
+    int tribonacci(int n) {
+        vector<int> dp(n+1, -1);
+        int ans = solve(n, dp);
+        return ans;
     }
 };
