090f953d44aa01be5f04f82cd94d4e8f9760e42a
Ajay Patel <155143901+ajaypatelxyz@users.noreply.github.com>
2026-08-26 11:33:17 +0530

    Time: 4 ms (19.14%), Space: 8.9 MB (37.53%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        // deque<string> dq;
-        // for(int i = 0; i < s.length(); i++){
-        //     if(!dq.empty() && (dq.front() == '1' || dq.front() == '0') && count == k){
-        //         dq.pop_front();
-        //     }else{
-        //         if(s[i] == '1') count++;
-        //         dq.push_back(s[i]);
-        //     }
-        // }
         int left = 0;
         int right = 0;
         int count = 0;
@@ -17,6 +8,7 @@ public:
         int start = 0;
         int end = 0;
         bool found = false;
+        
         while(right < s.length()){
             if(s[right] == '1'){
                 count++;
