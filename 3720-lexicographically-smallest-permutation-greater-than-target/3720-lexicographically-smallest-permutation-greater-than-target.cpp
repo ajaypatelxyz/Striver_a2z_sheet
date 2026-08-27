c2d3c75343cb0a398357d41fcc027b2c1a14ad06
Ajay Patel <155143901+ajaypatelxyz@users.noreply.github.com>
2026-08-27 15:43:00 +0530

    Time: 0 ms (100%), Space: 9.9 MB (82.17%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,73 +1,73 @@
 class Solution {
 public:
     string lexGreaterPermutation(string s, string target) {
-    int n = s.size();
+        int n = s.size();
 
-    // Frequency of characters in s
-    vector<int> freq(26, 0);
+        // Frequency of characters in s
+        vector<int> freq(26, 0);
 
-    for(char ch : s) {
-        freq[ch - 'a']++;
-    }
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
 
-    // temp = remaining characters while matching target
-    vector<int> temp = freq;
+        // temp = remaining characters while matching target
+        vector<int> temp = freq;
 
-    int best = -1;
+        int best = -1;
 
-    // Try to match target from left to right
-    for(int i = 0; i < n; i++) {
+        // Try to match target from left to right
+        for (int i = 0; i < n; i++) {
 
-        int curr = target[i] - 'a';
+            int curr = target[i] - 'a';
 
-        // Can we make this position greater?
-        for(int c = curr + 1; c < 26; c++) {
-            if(temp[c] > 0) {
-                best = i;
+            // Can we make this position greater?
+            for (int c = curr + 1; c < 26; c++) {
+                if (temp[c] > 0) {
+                    best = i;
+                    break;
+                }
+            }
+
+            // Cannot continue matching target
+            if (temp[curr] == 0) {
+                break;
+            }
+
+            temp[curr]--;
+        }
+
+        // No position can be made greater
+        if (best == -1) {
+            return "";
+        }
+
+        string ans = "";
+
+        // Put target prefix before best
+        for (int i = 0; i < best; i++) {
+            ans += target[i];
+            freq[target[i] - 'a']--;
+        }
+
+        // Put smallest character greater than target[best]
+        int curr = target[best] - 'a';
+
+        for (int c = curr + 1; c < 26; c++) {
+            if (freq[c] > 0) {
+                ans += char('a' + c);
+                freq[c]--;
                 break;
             }
         }
 
-        // Cannot continue matching target
-        if(temp[curr] == 0) {
-            break;
+        // Put remaining characters in sorted order
+        for (int c = 0; c < 26; c++) {
+            while (freq[c] > 0) {
+                ans += char('a' + c);
+                freq[c]--;
+            }
         }
 
-        temp[curr]--;
+        return ans;
     }
-
-    // No position can be made greater
-    if(best == -1) {
-        return "";
-    }
-
-    string ans = "";
-
-    // Put target prefix before best
-    for(int i = 0; i < best; i++) {
-        ans += target[i];
-        freq[target[i] - 'a']--;
-    }
-
-    // Put smallest character greater than target[best]
-    int curr = target[best] - 'a';
-
-    for(int c = curr + 1; c < 26; c++) {
-        if(freq[c] > 0) {
-            ans += char('a' + c);
-            freq[c]--;
-            break;
-        }
-    }
-
-    // Put remaining characters in sorted order
-    for(int c = 0; c < 26; c++) {
-        while(freq[c] > 0) {
-            ans += char('a' + c);
-            freq[c]--;
-        }
-    }
-
-    return ans;
-}
 };
