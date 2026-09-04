38eefc7f97150834d76f794af869590e4c5f847d
Ajay Patel <155143901+ajaypatelxyz@users.noreply.github.com>
2026-09-04 12:00:25 +0530

    Time: 0 ms (100%), Space: 31.5 MB (36.87%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> instability;
+        vector<int> prefixMax(n);
+        vector<int> sufixMin(n);
+        
+        prefixMax[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            prefixMax[i] = max(prefixMax[i - 1], nums[i]);
+        }
+
+        sufixMin[n - 1] = nums[n - 1];
+        for(int i = n - 2; i >= 0; i--){
+            sufixMin[i] = min(sufixMin[i + 1], nums[i]);
+        }
 
         for(int i = 0; i < n; i++){
-            int maxi = INT_MIN;
-            int mini = INT_MAX;
-            for(int j = 0; j <= i; j++){
-                maxi = max(maxi, nums[j]);
-            }
-            for(int j = i; j < n; j++){
-                mini = min(mini, nums[j]);
-            }
-            int val = maxi - mini;
-            instability.push_back(val);
-        }
-        
-        for(int i = 0; i < n; i++){
-            if(instability[i] <= k){
+            int instability = prefixMax[i] - sufixMin[i];
+            if(instability <= k){
                 return i;
             }
         }
