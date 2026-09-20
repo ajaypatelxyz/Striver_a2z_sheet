072767d286db4f5522408fe60aa3b86c0173b222
Ajay Patel <155143901+ajaypatelxyz@users.noreply.github.com>
2026-09-20 16:09:31 +0530

    Time: 3 ms (16.14%), Space: 9.6 MB (78.4%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -3,9 +3,7 @@ public:
     int reverseDegree(string s) {
         int sum = 0;
         for(int i = 1; i <= s.length(); i++){
-            int val = 26 - (s[i-1] - 'a');
-            int mul = val * i;
-            sum += mul;
+            sum += i * (26 - (s[i - 1] - 'a'));
         }
         return sum;
     }
